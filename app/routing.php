a13284294d470635249f3b0de6824ab0d52a617e
Tiago André <tandre@moonlight.lan>
2015-12-28 15:19:53 +0000

    blade and start

--- app/routing.php
+++ app/routing.php
@@ -1,3 +1,7 @@
 <?php
-$router->map('GET','/', 'home', 'home_index');
+/**
+ * This is the routing file, is the starting point of the app in this example we say that when the user go to the page / we load a controller called start.php that is in
+ * /app/controllers/start.php and run a function called index
+ */
+$router->map('GET','/', 'start.index', 'start_index');
  ?>
--- core/controller.php
+++ core/controller.php
@@ -2,6 +2,7 @@
 use Doctrine\ORM\EntityManager;
 class controller{
   protected $em;
+  protected $view;
   /**
  * @InjectParams({
  *    "em" = @Inject("doctrine.orm.entity_manager")
@@ -9,7 +10,16 @@ class controller{
  */
   public function __construct(EntityManager $em)
   {
-      $this->em = $em;
-      $this->blade = $blade;
+    $path = ['app/views'];         // your view file path, it's an array
+    $cachePath = 'tmp/cache';     // compiled file path
+
+    $compiler = new \Xiaoler\Blade\Compilers\BladeCompiler($cachePath);
+    $engine = new \Xiaoler\Blade\Engines\CompilerEngine($compiler);
+    $finder = new \Xiaoler\Blade\FileViewFinder($path);
+    $factory = new \Xiaoler\Blade\Factory($engine, $finder);
+    $this->view = $factory;
+
+    $this->em = $em;
+    $this->blade = $blade;
   }
 }
