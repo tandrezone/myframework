d2961cfaf4b09f919b9ead8a0543aec64c9afe5e
Tiago André <tandre@moonlight.lan>
2016-01-07 16:05:32 +0000

    base views now have access

--- config/configAccessViews.php
+++ config/configAccessViews.php
@@ -1,4 +1,4 @@
 <?php
 //Access views
 //here you put the folders with views you wanna have access in any controller
-define ("VIEWS", serialize (array ('packages/moonlight/errors/views')));
+define ("VIEWS", serialize (array ('packages/moonlight/errors/views', 'packages/moonlight/base/views')));
